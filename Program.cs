94890b33e34d360af9d8b12c121d70ffa9b27809
Amanda Brown <amanda.brown@gmail.com>
2016-09-08 20:15:56 -0400

    loop de loop

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //print out odd nums from 1-99 inclusive
+            //start at one, make condition <=99, add 2 after each loop
+            int counter;
+            for (counter = 1; counter <=99; counter += 2)
+            {
+                Console.WriteLine(counter);
+            }
+        }
+    }
+}
